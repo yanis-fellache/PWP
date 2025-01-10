3a71428a72e38ffa895d5750d26d17eadd9e841e
yanis-fellache <122501964+yanis-fellache@users.noreply.github.com>
2025-01-10 08:48:33 -0500

    Update Circle_detection/main.py

--- CircleDetection/main.py
+++ CircleDetection/main.py
@@ -10,12 +10,12 @@ def process_image(img):
     # Convert the image to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Apply a blur to the grayscale image
-    gray = cv2.blur(gray, (1, 1))
+    gray = cv2.blur(gray, (3, 3))
     # Apply a median blur to the grayscale image
     gray = cv2.medianBlur(gray, 1)
     gray = cv2.GaussianBlur(gray, (9, 9), 0)
     edges = cv2.Canny(gray, 30, 200)
-
+    # apply mask
     return edges
 
 
@@ -24,7 +24,7 @@ def detect_circles(processed_img):
     rows = processed_img.shape[0]
 
     # Use HoughCircles to detect circles in the processed image
-    circles = cv2.HoughCircles(processed_img, cv2.HOUGH_GRADIENT, dp=1, minDist=rows / 8, param1=100, param2=140, minRadius=150, maxRadius=1500)
+    circles = cv2.HoughCircles(processed_img, cv2.HOUGH_GRADIENT, dp=1, minDist=rows / 8, param1=100, param2=140)
     return circles
 
 
@@ -50,10 +50,10 @@ def draw_circle(img, circles):
 
 
 def main():
-    frame = cv2.imread("can.jpg")
+    frame = cv2.imread("can4.jpg")
     process_frame = process_image(frame)
-    # cv2.imshow("Circles", process_frame)
-    # cv2.waitKey(0)
+    cv2.imshow("Circles", process_frame)
+    cv2.waitKey(0)
     circles = detect_circles(process_frame)
     img_with_circle = draw_circle(frame, circles)
     cv2.imshow("Circles", img_with_circle)
